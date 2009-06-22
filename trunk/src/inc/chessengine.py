089411168ab800ebff293d501639ba931075fac8
vinchkovsky1 <vinchkovsky1@a6ed8a46-56b9-11de-b31f-81d54249e7d9>
2009-06-22 17:32:04 +0000

    #

--- trunk/src/inc/chessengine.py
+++ trunk/src/inc/chessengine.py
@@ -1868,9 +1868,9 @@ def clearLine(line):
                 break
 
     #clear moves after checkmate
-#    if gameLine.find('#')!=len(gameLine)-1:
-#        gameLine = gameLine[:gameLine.find('#')+1]
-#        gameLineCorrected = 1
+    if gameLine.find('#')>0 and gameLine.find('#')!=len(gameLine)-1:
+        gameLine = gameLine[:gameLine.find('#')+1]
+        gameLineCorrected = 1
 
     return gameLine
 
